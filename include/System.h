80823ed23c4d5f8092ed3bfecd92923de791f532
George Harker <george@george-graphics.co.uk>
2023-07-17 19:44:27 -0700

    increase I2c fifo

--- include/System.h
+++ include/System.h
@@ -81,8 +81,8 @@ protected:
 
 #if CONFIG_I2C_SLAVE
 	enum {
-		I2C_SLAVE_OUT_FIFO_ORDER = 5,
-		I2C_SLAVE_IN_FIFO_ORDER = 5,
+		I2C_SLAVE_OUT_FIFO_ORDER = 7,
+		I2C_SLAVE_IN_FIFO_ORDER = 7,
 	};
 	
 	uint8_t m_I2CSlaveOutFifoStor[1 << I2C_SLAVE_OUT_FIFO_ORDER];
